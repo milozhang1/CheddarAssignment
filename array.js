34172855b909c4d3103c4fc3ecbafe37650d4a3b
milo.zhang <miloshanghai@gmail.com>
2020-01-19 23:59:24 -0500

    first commit

--- array.js
+++ array.js
@@ -1,8 +1,28 @@
 Array.prototype.find = function(fn) {
-}
+ for (var i = 0; i < this.length; i++) {
+        if (fn(this[i])) {
+            return this[i];
+        }
+    }
+    
+    return undefined;
+};
 
 Array.prototype.findIndex = function(fn) {
-}
+
+    for (var i = 0; i < this.length; i++) {
+        if (fn(this[i]))
+            return i;
+    }
+    
+    return -1;
+};
 
 Array.prototype.includes = function(search) {
-}
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] === search)
+            return true;
+    }
+    
+    return false;
+};
--- string.js
+++ string.js
@@ -1,2 +1,6 @@
 String.prototype.endsWith = function(search, position) {
-}
+        if (position === undefined || position >= this.length) {
+			position = this.length;
+		}
+        return this.substring(position - search.length, position) === search;
+};
